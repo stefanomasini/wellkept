6704fb5451922cf086e98434a96dd0c44d543648
Stefano Masini <stefano@stefanomasini.com>
2022-06-06 17:38:50 +0200

    fix: removed fs/promises to support older versions of node

--- src/filesystem.ts
+++ src/filesystem.ts
@@ -1,21 +1,40 @@
-import { readFile, writeFile, access } from 'fs/promises';
+import { readFile, writeFile, access } from 'fs';
 import { FileSystem } from './types';
 
 export class RealFileSystem implements FileSystem {
-    async writeVaultFile(filepath: string, encryptedContent: string): Promise<void> {
-        await writeFile(filepath, encryptedContent);
+    writeVaultFile(filepath: string, encryptedContent: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            writeFile(filepath, encryptedContent, (err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        });
     }
 
-    async readVaultFile(filepath: string): Promise<string> {
-        return await readFile(filepath, { encoding: 'utf8' });
+    readVaultFile(filepath: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            readFile(filepath, { encoding: 'utf8' }, (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data);
+                }
+            });
+        });
     }
 
-    async checkFileExists(filepath: string): Promise<boolean> {
-        try {
-            await access(filepath);
-            return true;
-        } catch (err) {
-            return false;
-        }
+    checkFileExists(filepath: string): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            access(filepath, (err) => {
+                if (err) {
+                    resolve(false);
+                } else {
+                    resolve(true);
+                }
+            });
+        });
     }
 }
